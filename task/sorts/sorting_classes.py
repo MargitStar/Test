32f8a604861a61a025eb1683308e4d54664ecf43
Margarita <sovenokbymargo@mail.ru>
2021-03-02 13:54:14 +0300

    Add Insertion sort

--- task/sorts/sorting_classes.py
+++ task/sorts/sorting_classes.py
@@ -27,8 +27,20 @@ class OptimizedBubbleSort(Sort):
         while has_swapped:
             has_swapped = False
             for i in range(len(sorted_array) - 1):
-                if sorted_array[i] > sorted_array[i+1]:
+                if sorted_array[i] > sorted_array[i + 1]:
                     sorted_array[i], sorted_array[i + 1] = sorted_array[i + 1], sorted_array[i]
                     has_swapped = True
 
+        return sorted_array
+
+
+class InsertionSort(Sort):
+    def sorted(self):
+        sorted_array = self.array[:]
+        for i in range(1, len(sorted_array)):
+            j = i - 1
+            while j >= 0 and sorted_array[i] < sorted_array[j]:
+                sorted_array[j + 1] = sorted_array[j]
+                j -= 1
+            sorted_array[j + 1] = sorted_array[i]
         return sorted_array
