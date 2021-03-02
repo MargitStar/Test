902a28ff7fe6c258145fbbd214c51b8ea43ed4c8
Margarita <sovenokbymargo@mail.ru>
2021-03-02 18:26:25 +0300

    Add serach by type field in Admin

--- task/sorts/models.py
+++ task/sorts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib import admin
 
-# Create your models here.
+
+class Sort(models.Model):
+    initial = models.FileField(
+        'Initial file',
+    )
+
+    result = models.FileField(
+        'Result file'
+    )
+
+    speed = models.CharField(
+        'Execution time',
+        max_length=100,
+        default='',
+    )
+
+    type = models.CharField(
+        'Type',
+        max_length=100,
+        default='',
+    )
+
+    def __str__(self):
+        return f"Object {self.pk}"
+
+
+class SearchSortType(admin.ModelAdmin):
+    search_fields = ['type']
