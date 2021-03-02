f6622b3f2c8c2832b035bc8e2c1cd5c7f9b44d8b
Margarita <sovenokbymargo@mail.ru>
2021-03-02 13:09:47 +0300

    Add BubbleSort class

--- task/sorts/sorting_classes.py
+++ task/sorts/sorting_classes.py
@@ -1,11 +1,23 @@
 from abc import ABC, abstractmethod
 
 
-class Sorting(ABC):
+class Sort(ABC):
     def __init__(self, array):
         self.array = array
 
     @abstractmethod
     def sorted(self):
-        return sorted(self.array)
+        pass
 
+
+class BubbleSort(Sort):
+    def sorted(self):
+        sorted_array = self.array[:]
+        for i in range(len(sorted_array)):
+            for j in range(len(sorted_array) - 1):
+                if sorted_array[j] > sorted_array[j+1]:
+                    sorted_array[j], sorted_array[j+1] = sorted_array[j+1], sorted_array[j]
+        return sorted_array
+
+
+clas
