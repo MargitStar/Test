045484bf44763fe77dde2f203fa729e913716b90
margitstar <sovenokbymargo@mail.ru>
2021-03-03 15:43:15 +0300

    Add crispy_forms

--- task/task/settings.py
+++ task/task/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sorts'
+    'sorts',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
